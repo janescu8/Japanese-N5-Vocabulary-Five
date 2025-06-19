2a9b7ffd96313d1306f6a64d80d56d0e3a3d8d9c
janescu8 <97823761+janescu8@users.noreply.github.com>
2025-06-19 15:29:59 +0800

    Create jun_19_a.py

---
+++ jun_19_a.py
@@ -0,0 +1,8 @@
+word_data = {
+    "盛り上がる": ("to get excited; to liven up", "パーティーは夜遅くまで盛り上がりました。"),
+    "盛り上がり": ("excitement; climax", "ライブの盛り上がりが最高潮に達した。"),
+    "食べ放題": ("all-you-can-eat", "このレストランは寿司の食べ放題があります。"),
+    "居酒屋": ("Japanese-style pub", "仕事の後、同僚と居酒屋に行きました。"),
+    "完璧": ("perfect; flawless", "彼のプレゼンは完璧だった。"),
+    "選択": ("choice; selection", "大学ではさまざまな専攻の選択があります。"),
+}
